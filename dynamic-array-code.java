6d5a79c2428466642e866ce9e8d304c9d4d299be
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-03-26 08:35:35 +0500

    learning

---
+++ dynamic-array-code.java
@@ -0,0 +1,65 @@
+@SuppressWarnings("unchecked")
+public class Array <T> implements Iterable <T>{
+
+    private T [] arr; 
+    private int len = 0;       // Length user thinks array is
+    private int capacity = 0;  // Actual Array Size
+
+    public Array() {this(16);}
+
+    public Array(int capacity){
+
+        if(capacity < 0) throw new IllegalArgumentException("Illegal Capacity: " + capacity);
+        this.capacity = capacity;
+        arr = (T[]) new Object[capacity];
+    }
+
+    public int size() {return len;}
+    public boolean isEmpty() { return size() == 0; }
+
+    public T get(int index) { return arr[index]; }
+    public void set(int index, T elem) {arr[index] = elem; }
+
+    public void clear(){
+
+        for(int i = 0; i < capacity; i++)
+        arr[i] = null;
+        len = 0;
+    }
+
+}
+
+public void add(T elem){
+
+    if(len + 1 >= capacity){
+        if(capacity == 0) capacity = 1;
+        else capacity *= 2;
+        T[] new arr = (T[]) new Object[capacity];
+        for (int i = 0; i < len; i++)
+        new_arr[i] = arr[i];
+        arr = new_arr;
+    }
+
+    arr[len++] = elem;
+
+}
+
+
+//Remove the element at the specified index in this list.
+public T removeAt(int rm_index){
+
+    if(rm_index >= len && rm_index < 0) throw new IndexOutOfBoundsException();
+    T data = arr[rm_index];
+    T[] new_arr = (T[]) new Object[len - 1];
+    for (int i = 0, j = 0; i < len; i++, j++)
+    if(i == rm_index) j--;  //skip over rm_index by fixing j temporarily
+    else new_arr[j] = arr[i];
+    arr = new_arr;
+    capacity = --len;
+    return data;
+}
+
+public boolean remove(Object obj){
+
+    
+}
