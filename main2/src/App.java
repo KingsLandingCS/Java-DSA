515d7c0c7ff9a0ddaf17b4bd890f8475e2b922d5
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-03-30 10:05:47 +0500

    learning

--- main2/src/App.java
+++ main2/src/App.java
@@ -5,13 +5,16 @@ public class App {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("what is your name?");
+        System.out.println("what is your name? ");
         String name = scanner.nextLine();
         System.out.println("How old are you? ");
         int age = scanner.nextInt();
-        System.out.println(name);
+        scanner.nextLine();
+        System.out.println("What is your favortie food");
+        String food = scanner.nextLine();
 
         System.out.println("Hello " + name);
         System.out.println("You're " + age + " years old");
+        System.out.println("You like " + food);
     }
 }
