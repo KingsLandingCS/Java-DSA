13f5d2d64038e56d5bce0bdac27533ec05a1766d
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-03-30 10:42:16 +0500

    learning

---
+++ main3/main3/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        double friends = 10;
+
+        friends = (double) friends / 3;
+
+        System.out.println(friends);
+
+    }
+
+}
