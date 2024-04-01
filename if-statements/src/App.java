43ad7f11d4ff653c296390edf0b47c4b328d06f7
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-04-01 21:44:10 +0500

    learning

---
+++ if-statements/src/App.java
@@ -0,0 +1,20 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        int age = 75;
+
+        if (age >= 18) {
+
+            System.out.println("You're are an adult!");
+        } else if (age >= 75) {
+
+            System.out.println("Okay boomer!");
+
+        } else {
+
+            System.out.println("You're not an adult!");
+        }
+
+    }
+}
